a8b20868c7c9a640de82c60bc6c0f14327ed66ba
Othmane AIT EL CADI <dartzon@gmail.com>
2018-07-29 12:01:32 +0100

    Add bash script to generate .dir-locals.el file

---
+++ .generate-dir-locals.sh
@@ -0,0 +1,20 @@
+²#!/bin/bash
+
+_build_mode=$1
+_build_arch=$2
+_project_path=$3
+_project_name=$4
+_build_args=$5
+
+_gcc_version=$(gcc -dumpversion)
+
+_dir_locals="(( nil . (\n\t
+(flycheck-clang-tidy-build-path . \"$_project_path/build/$_build_mode/$_build_arch/\")\n\t
+(projectile-enable-caching . t)\n\t
+(projectile-project-name . \"$_project_name\")\n\t
+(projectile-project-root . \"$_project_path/\")\n\t
+)\n\t
+)
+(c-mode . ((mode . c++))) )\n\t"
+
+echo -e $_dir_locals > $_project_path/.dir-locals.el
