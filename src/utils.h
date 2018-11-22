1695a0d0296bf13ffad721c2fce8baf278a43114
Othmane AIT EL CADI <dartzon@gmail.com>
2018-11-22 13:52:01 +0100

    Add CPU ops print function for debugging

--- src/utils.h
+++ src/utils.h
@@ -31,6 +31,8 @@
 // std includes.
 #include <cstdint>
 
+#include "units.h"
+
 #ifdef COLORBOY_DEBUG
 
 ////////////////////////////////////////////////////////
@@ -48,24 +50,28 @@ assertWithMessage(const bool test, const char* fctName, const int lineNum, const
 {
     if (test == false)
     {
-        printf("[ASSERTION FAILED] - [%s @ Line %d] : %s\n", fctName, lineNum, errorMsg);
+        printf("\033[1;31m[ASSERTION FAILED] - [%s @ Line %d] : %s\033[0m\n",
+               fctName,
+               lineNum,
+               errorMsg);
+        exit(1);
     }
 }
 
 inline void logMessage(const char* fctName, const int lineNum, const char* msg)
 {
-    printf("[INFO] - [%s @ Line %d] : %s\n", fctName, lineNum, msg);
+    printf("\033[1;34m[INFO] - [%s @ Line %d] : %s\033[0m\n", fctName, lineNum, msg);
 }
 
-/// \def  GBASSERT(TEST, MSG)
+/// \def  CBASSERT(TEST, MSG)
 ///
 /// Checks assertion based on the result of @a TEST and displays @a MSG.
-#define GBASSERT(TEST, MSG) assertWithMessage(TEST, __func__, __LINE__, "\n\t> DETAILS : " MSG)
+#define CBASSERT(TEST, MSG) assertWithMessage(TEST, __func__, __LINE__, "\n\t> DETAILS : " MSG)
 
-/// \def  GBLOG(MSG)
+/// \def  CBLOG(MSG)
 ///
 /// Displays information message in @a MSG.
-#define GBLOG(MSG) assertWithMessage(__func__, __LINE__, "\n\t> DETAILS : " MSG)
+#define CBLOG(MSG) logMessage(__func__, __LINE__, "\n\t> DETAILS : " MSG)
 
 /// \def  NOTYETIMPLEMENTED()
 ///
@@ -73,11 +79,74 @@ inline void logMessage(const char* fctName, const int lineNum, const char* msg)
 #define NOTYETIMPLEMENTED()                                                                        \
     assertWithMessage(false, __func__, __LINE__, "Function not yet implemented!\n")
 
-#else
+template<typename T = uint8_t>
+void printCPUOp(const uint16_t addr,
+                const std::string& op,
+                const uint16_t ir,
+                const uint16_t pc,
+                const std::initializer_list<T>& arg)
+{
+    std::string comment;
+
+    switch (ir)
+    {
+    case 0x18:
+    case 0x20:
+    case 0x28:
+    case 0x30:
+    case 0x38:
+    case 0xC2:
+    case 0xC3:
+    case 0xC4:
+    case 0xCA:
+    case 0xCC:
+    case 0xCD:
+    case 0xD2:
+    case 0xD4:
+    case 0xDA:
+    case 0xDC:
+    case 0xE9: comment = "\t; Jump to @0x%04x"; break;
+
+    case 0xC0:
+    case 0xC8:
+    case 0xC9:
+    case 0xD0:
+    case 0xD8:
+    case 0xD9: comment = "\t; Return to @0x%04x"; break;
+    };
+
+    const std::string format = std::string("\033[1;33m0x%04x\033[0m\t ") + op + "\033[1;32m" +
+                               comment + "\033[0m\n";
+
+    if (arg.size() > 0)
+    {
+        const T val = *std::cbegin(arg);
+
+        printf(format.c_str(), addr, val, pc);
+    }
+    else
+    {
+        printf(format.c_str(), addr, pc);
+    }
+}
+
+/// \def  PRINTOP(OP, ARG)
+///
+/// Prints the current CPU instruction being executed.
+/// Only use withing the Cpu class as it uses its member variables.
+#define PRINTOP(OP, ARG)                                                                           \
+    printCPUOp(m_currentInstructionAddr - static_cast<const uint8_t>(m_inPrefixCBOp),              \
+               OP,                                                                                 \
+               IR,                                                                                 \
+               PC,                                                                                 \
+               ARG)
+
+#else  // Not COLORBOY_DEBUG.
 
 #define CBASSERT(TEST, MSG)
 #define CBLOG(MSG)
 #define NOTYETIMPLEMENTED()
+#define PRINTOP(OP, ARG)
 
 #endif
 
@@ -105,6 +174,28 @@ inline void splitWord(const uint16_t word, uint8_t& lByte, uint8_t& hByte)
 {
     lByte = word & 0xFF;
     hByte = word >> 8;
+}
+
+/// \brief Convert a data unit literal to a numeric byte value.
+///
+/// \param val data unit literal.
+///
+/// \return numeric byte value.
+template<typename DataType>
+constexpr uint32_t toByteValue(const DataType val)
+{
+    return static_cast<uint32_t>(static_cast<units::data::byte_t>(val));
+}
+
+/// \brief Convert a frequency unit literal to a numeric Hertz value.
+///
+/// \param val frequency unit literal.
+///
+/// \return numeric Hertz value.
+template<typename DataType>
+constexpr uint32_t toHzValue(const DataType val)
+{
+    return static_cast<uint32_t>(static_cast<units::frequency::hertz_t>(val));
 }
 
 }  // namespace cbutil
