2c098aa085fabec763cfa7b244269268dd54229a
Othmane AIT EL CADI <dartzon@gmail.com>
2018-11-22 13:49:01 +0100

    Add memory mapping functionality to the MMU

--- src/mmu.h
+++ src/mmu.h
@@ -28,9 +28,7 @@
 #ifndef MMU_H_
 #define MMU_H_
 
-#include <cstdint>
 #include <array>
-#include <vector>
 #include <algorithm>
 
 #include "config.h"
@@ -39,76 +37,82 @@
 class Mmu
 {
 public:
-    /// \brief Constructor.
-    ///
-    /// \param memory std::array representing the Game Boy's internal memory.
-    explicit Mmu(std::array<uint8_t, GBConfig::sysMemSize>& memory) : m_memory(memory) {}
-
     /// \brief Read byte from memory address.
     ///
     /// \param address Memory address to read from.
     ///
     /// \return Byte located at the given address.
-    uint8_t readByte(const uint16_t address) const;
+    uint8_t readByte(const uint16_t address) const
+    {
+        CBASSERT((address >= 0) && (address <= m_memoryMap.size() - 1),
+                 "Out of bounds memory read");
+
+        return *m_memoryMap[address];
+    }
 
     /// \brief Read word from memory address.
     ///
     /// \param address Memory address to read from.
     ///
     /// \return Word located at the given address.
-    uint16_t readWord(const uint16_t address) const;
+    uint16_t readWord(const uint16_t address) const
+    {
+        CBASSERT((address >= 0) && (address <= m_memoryMap.size() - 1),
+                 "Out of bounds memory read");
+
+        return cbutil::combineTwoBytes(*m_memoryMap[address], *m_memoryMap[address + 1]);
+    }
 
     /// \brief Write byte to memory address.
     ///
     /// \param byte Value to write.
     /// \param address Memory address to write into.
-    void writeByte(const uint8_t byte, const uint16_t address);
+    void writeByte(const uint8_t byte, const uint16_t address)
+    {
+        CBASSERT((address >= 0) && (address <= m_memoryMap.size() - 1),
+                 "Out of bounds memory write");
+
+        *m_memoryMap[address] = byte;
+    }
 
     /// \brief Write word to memory address.
     ///
     /// \param word Value to write.
     /// \param address Memory address to write into
-    void writeWord(const uint16_t word, const uint16_t address);
-
-    /// \brief Map data from a buffer to the internal RAM.
-    ///
-    /// \param buffer buffer to map.
-    /// \param startAddr start address where the data should be copied to.
-    template<std::size_t byteCount>
-    void mapDataBufferToMemory(const std::array<uint8_t, byteCount>& buffer, uint16_t startAddr)
+    void writeWord(const uint16_t word, const uint16_t address)
     {
-        std::copy_n(std::cbegin(buffer), byteCount, std::begin(m_memory) + startAddr);
+        CBASSERT((address >= 0) && (address <= m_memoryMap.size() - 1),
+                 "Out of bounds memory write");
+
+        *m_memoryMap[address] = word & 0xFF;
+        *m_memoryMap[address + 1] = word >> 8;
     }
 
     /// \brief Map data from a buffer to the internal RAM.
     ///
     /// \param buffer buffer to map.
     /// \param startAddr start address where the data should be copied to.
-    void mapDataBufferToMemory(const std::vector<uint8_t>& buffer, uint16_t startAddr)
+    template<typename Container>
+    void mapDataBufferToMemory(Container& buffer, const uint16_t startAddr)
     {
-        std::copy_n(std::cbegin(buffer), buffer.size(), std::begin(m_memory) + startAddr);
+        for (size_t idx = 0; idx < buffer.size(); ++idx)
+        {
+            m_memoryMap[startAddr + idx] = &buffer[idx];
+        }
     }
 
-    enum MemoryAreas : uint16_t
+    template<class InputIt>
+    void mapDataBufferToMemory(InputIt first, InputIt last, const uint16_t startAddr)
     {
-        eWorkRAMStart = 0xC000,
-        eWorkRAMEnd = 0xDFFF,
-
-        eEchoRAMStart = 0xE000,
-        eEchoRAMEnd = 0xFDFF,
-
-        eHardwareIORegsStart = 0xFF00,
-        eHardwareIORegsEnd = 0xFF7F
-    };
-
-    enum HardwareIORegisters : uint16_t
-    {
-        eHIO_RomSwitch = 0xFF50
-    };
+        const size_t memSize = std::distance(first, last);
+        for (size_t idx = 0; idx < memSize; ++idx)
+        {
+            m_memoryMap[startAddr + idx] = &(*(first + idx));
+        }
+    }
 
 private:
-    std::array<uint8_t,
-               GBConfig::sysMemSize>& m_memory;  ///< Representation of the console's memory.
+    std::array<uint8_t*, GBConfig::memorySize> m_memoryMap;
 };
 
 #endif /* MMU_H_ */
