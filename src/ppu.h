0b806869a82fb377e0827a46322a200b0ecd07f1
Othmane AIT EL CADI <dartzon@gmail.com>
2018-11-22 14:12:19 +0100

    First commit of the Game Boy's PPU

---
+++ src/ppu.h
@@ -0,0 +1,45 @@
+/// Copyright (c) 2018 - present    Othmane AIT EL CADI <dartzon@gmail.com>
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+
+/// \file      ppu.h
+///
+/// \brief     <brief description>
+///
+/// \author    Othmane AIT EL CADI - <dartzon@gmail.com>
+/// \date      12-11-2018
+
+#ifndef PPU_H_
+#define PPU_H_
+
+#include "mmu.h"
+
+#include "lcd.h"
+
+class Ppu
+{
+    // public:
+    //     void run(const uint32_t currentCPUCycle);
+
+    // private:
+    //     void scanOAM(const uint8_t scanLine);
+    //     void transferPixels(const Lcd& lcd);
+};
+
+#endif /* PPU_H_ */
